6cc05aee3ab264fd6b192413eef5aad748486dcd
Dillon M. Baird <dillonbaird@outlook.com>
2025-02-20 20:17:18 -0800

    fix video bg

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -1983,7 +1983,7 @@
 						autoplay
 						class="object-cover absolute top-0 left-0 w-full h-full"
 					></video>
-					<div class="absolute inset-0 bg-black bg-opacity-10"></div>
+					<div class="absolute inset-0 bg-black" style="opacity: 0.1;"></div>
 					{#if $settings?.landingPageMode === 'chat' || createMessagesList(history, history.currentId).length > 0}
 						<div
 							class=" pb-2.5 flex flex-col justify-between w-full flex-auto overflow-auto h-0 max-w-full z-10 scrollbar-hidden"
